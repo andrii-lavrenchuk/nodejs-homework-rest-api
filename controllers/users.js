2c6a994239cbbfd89e08774dd6e4a772920d242c
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-03-11 21:46:46 +0200

    Adds saveAvatarToStatic function

--- controllers/users.js
+++ controllers/users.js
@@ -79,28 +79,7 @@ const logout = async (req, res, _next) => {
 const avatars = async (req, res, _next) => {
   try {
     const id = req.user.id;
-    const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
-    const pathFile = req.file.path;
-    const newNameAvatar = `${Date.now()}-${req.file.originalname}`;
-    const img = await Jimp.read(pathFile);
-    await img
-      .autocrop()
-      .cover(
-        250,
-        250,
-        Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE,
-      )
-      .writeAsync(pathFile);
-    await createFolderIsExist(path.join(AVATARS_OF_USERS, id));
-    await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar));
-    const avatarUrl = path.normalize(path.join(id, newNameAvatar));
-    try {
-      await fs.unlink(
-        path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar),
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
+    const avatarUrl = await saveAvatarToStatic(req);
     await Users.updateAvatar(id, avatarUrl);
     return res.json({
       status: 'Success',
@@ -112,6 +91,30 @@ const avatars = async (req, res, _next) => {
   } catch (error) {
     next(error);
   }
+};
+
+const saveAvatarToStatic = async req => {
+  const id = req.user.id;
+
+  const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
+  const pathFile = req.file.path;
+  const newNameAvatar = `${Date.now()}-${req.file.originalname}`;
+  const img = await Jimp.read(pathFile);
+  await img
+    .autocrop()
+    .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
+    .writeAsync(pathFile);
+  await createFolderIsExist(path.join(AVATARS_OF_USERS, id));
+  await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar));
+  const avatarUrl = path.normalize(path.join(id, newNameAvatar));
+  try {
+    await fs.unlink(
+      path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar),
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+  return avatarUrl;
 };
 
 module.exports = { reg, login, logout, avatars };
